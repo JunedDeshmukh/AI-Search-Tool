345796ad27b349296956820dd1de8cdfb35f902e
JunedDeshmukh <jdeshmukh077@gmail.com>
2025-08-12 11:25:19 +0530

    highlighting code part

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect, useRef } from "react";
 import { URL } from "./constants";
 import "./App.css";
-import Answer from "./components/Answers";
+import RecentSearch from "./components/RecentSearch";
+import QuestionAnswer from "./components/QuestionAnswer";
 
 function App() {
   const [question, setQuestion] = useState("");
@@ -38,7 +39,7 @@ function App() {
         },
       ],
     };
-     setLoader(true);
+    setLoader(true);
 
     let response = await fetch(URL, {
       method: "POST",
@@ -58,22 +59,16 @@ function App() {
     ]);
     setQuestion("");
 
-     setTimeout(() => {
+    setTimeout(() => {
       scrollToAns.current.scrollTop = scrollToAns.current.scrollHeight;
-
     }, 500);
 
-    setLoader(false)
+    setLoader(false);
 
     //  console.log(response.candidates[0].content.parts[0].text);
     // setResult(response.candidates[0].content.parts[0].text);
   };
   // console.log(recentHistory);
-
-  const clearHistory = () => {
-    localStorage.clear();
-    setRecentHistory([]);
-  };
 
   const isEnter = (event) => {
     if (event.key == "Enter") {
@@ -81,97 +76,49 @@ function App() {
     }
   };
 
-   useEffect(()=>{
+  useEffect(() => {
     console.log(selectedHistory);
     askQuestion();
-    
-  },[selectedHistory])
+  }, [selectedHistory]);
 
   return (
     <div className="grid grid-cols-5 h-screen text-center">
-      <div className="col-span-1 bg-zinc-800 pt-3">
-        <h1 className="text-xl text-white flex text-center justify-center">
-          <span>Recent Search</span>
-          <button
-            onClick={clearHistory}
-            className="cursor-pointer pl-3 align-middle"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="24px"
-              fill="#EFEFEF"
-            >
-              <path d="M312-144q-29.7 0-50.85-21.15Q240-186.3 240-216v-480h-48v-72h192v-48h192v48h192v72h-48v479.57Q720-186 698.85-165T648-144H312Zm336-552H312v480h336v-480ZM384-288h72v-336h-72v336Zm120 0h72v-336h-72v336ZM312-696v480-480Z" />
-            </svg>
-          </button>
-        </h1>
-        <ul className="text-left overflow-auto mt-2">
-          {recentHistory &&
-            recentHistory.map((item) => (
-              <li
-                onClick={()=>setSelectedHistory(item)}
-                className="pl-5 px-5 truncate text-zinc-400 cursor-pointer hover:bg-zinc-700
-              hover:text-zinc-200"
-              >
-                {item}
-              </li>
-            ))}
-        </ul>
-      </div>
+      <RecentSearch
+        recentHistory={recentHistory}
+        setRecentHistory={setRecentHistory}
+        setSelectedHistory={setSelectedHistory}
+      />
       <div className="col-span-4 p-8">
-        <h1 className='text-3xl bg-clip-text text-transparent bg-gradient-to-r from-pink-700 to-violet-700'
-        >Hello User, Ask me Anything</h1>
-        {
-          loader ?
-            <div role="status">
-              <svg aria-hidden="true" class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-              </svg>
-              <span class="sr-only">Loading...</span>
-            </div> : null
-        }
+        <h1 className="text-3xl bg-clip-text text-transparent bg-gradient-to-r from-pink-700 to-violet-700">
+          Hello User, Ask me Anything
+        </h1>
+        {loader ? (
+          <div role="status">
+            <svg
+              aria-hidden="true"
+              className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
+        ) : null}
 
-
-        <div ref={scrollToAns} className='container h-110 overflow-y-scroll'>
+        <div ref={scrollToAns} className="container h-110 overflow-y-scroll">
           <div className="text-zinc-300">
             <ul>
               {result.map((item, index) => (
-                <div
-                  key={index + Math.random()}
-                  className={item.type == "q" ? "flex justify-end" : ""}
-                >
-                  {item.type == "q" ? (
-                    <li
-                      key={index + Math.random()}
-                      className="text-right p-1 border-8 bg-zinc-700 border-zinc-700 rounded-tl-3xl rounded-br-3xl rounded-bl-3xl w-fit
-                   "
-                    >
-                      <Answer
-                        ans={item.text}
-                        totalResult={1}
-                        index={index}
-                        type={item.type}
-                      />
-                    </li>
-                  ) : (
-                    item.text.map((ansItem, ansIndex) => (
-                      <li
-                        key={ansIndex + Math.random()}
-                        className="text-left p-1"
-                      >
-                        <Answer
-                          ans={ansItem}
-                          totalResult={item.length}
-                          type={item.type}
-                          index={ansIndex}
-                        />
-                      </li>
-                    ))
-                  )}
-                </div>
+                <QuestionAnswer key={index} item={item} index={index} />
               ))}
             </ul>
           </div>
